1ccd6ef26209827151b16ff9fa8b8512a61a4997
Steelodessa <steelodessa@gmail.com>
2021-06-16 18:23:42 +0300

    ДЗ 5. Доп ДЗ — Объекты

---
+++ ДЗ_5/script/script.js
@@ -0,0 +1,33 @@
+//Task #1
+
+let obj = {};
+obj.name = "John";
+obj.surname = "Smith";
+//console.log(obj);
+obj.name = "Pete";
+//console.log(obj);
+delete obj.name;
+console.log(obj);
+
+//Task #2
+/* Можно изменять только свойства объекта, а не переменную,в которой находится объект
+user.name = «Pete»; - работать будет*/
+
+//Task #3
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+//1 самый простой ))
+let sumResult = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sumResult);
+
+//2 динамический
+let arr = Object.values(salaries); //получили массив arr и дальше перебрали
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log(sum);
