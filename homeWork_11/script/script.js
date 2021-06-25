82270ef20a362d23621064edc1b778f1ffa2442e
Steelodessa <steelodessa@gmail.com>
2021-06-25 15:11:08 +0300

    Уменьшение циклов

--- homeWork_11/script/script.js
+++ homeWork_11/script/script.js
@@ -1,5 +1,6 @@
 let numeric = +prompt("Введите число", "");
 let result = true;
+/*
 if (numeric > 1) {
   for (let i = 2; i < numeric; i++) {
     if (numeric % i === 0) {
@@ -11,3 +12,31 @@ if (numeric > 1) {
   result = false;
 }
 console.log(result);
+*/
+/*
+if (numeric > 1) {
+  let i = 2;
+  while (i <= numeric / 2) {
+    if (numeric % i === 0) {
+      result = false;
+      break;
+    } else {
+      i++;
+    }
+  }
+} else {
+  result = false;
+}
+console.log(result);
+*/
+if (isNaN(numeric) || numeric % 1 !== 0) {
+  result = false;
+} else {
+  var sqrtNum = Math.sqrt(numeric);
+  for (var i = 2; i <= sqrtNum; i++) {
+    if (numeric % i === 0) {
+      result = false;
+    }
+  }
+}
+console.log(sqrtNum, result);
