f58ecc382be19c6dad66b054e824aa73bd95f895
Steelodessa <steelodessa@gmail.com>
2021-07-14 20:31:16 +0300

    правки

--- homeWork_23/script/script.js
+++ homeWork_23/script/script.js
@@ -2,7 +2,7 @@
 //Task #1 Реализовать рекурсивную функцию которая находит факториал переданного в нее числа getFactorial(3) // в данном случае должна вернуть факториал числа 3! = 3 * 2 * 1
 
 function factorial(num) {
-  if (num === 1 || num === 0) {
+  if (num === 1) {
     return 1;
   } else {
     return num * factorial(num - 1);
@@ -12,7 +12,7 @@ console.log(factorial(5));
 
 //Используя тернарный оператор
 function factorialTwo(num) {
-  return num === 1 || num === 0 ? 1 : num * factorial(num - 1);
+  return num === 1 ? 1 : num * factorial(num - 1);
 }
 console.log(factorialTwo(6));
 
