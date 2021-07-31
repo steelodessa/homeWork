9b39db986c142cfbb7ca4a1b0b582638c3553a8e
Steelodessa <steelodessa@gmail.com>
2021-07-31 13:05:22 +0300

    hw_31

---
+++ hw_31/js/solution.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Получаем все li в документе
+let ulElement = document.getElementsByTagName("li");
+//console.log(ulElement2);
+
+for (let i of ulElement) {
+  console.log(i);
+}
+
+//Вывести общее кол-во элементов в консоль
+console.log("Count <li>: " + ulElement.length);
+
+//Вытянуть текст из тегов li и записать текст содержащийся в каждом элементе в массив — вывести этот массив в консоль
+function getArrayLi() {
+  let arrLi = [];
+  for (let i of ulElement) {
+    arrLi.push(i.innerHTML);
+  }
+  console.log(arrLi);
+}
+getArrayLi();
