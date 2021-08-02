211f4b2e1a7e462717d7e164651f7e8d318f6d2e
steelodessa <78382818+steelodessa@users.noreply.github.com>
2021-08-02 11:40:44 +0300

    Update solution.js

--- hw_31/js/solution.js
+++ hw_31/js/solution.js
@@ -4,17 +4,17 @@
 let liElement = document.getElementsByTagName("li");
 //console.log(ulElement);
 
-for (let i of ulElement) {
+for (let i of liElement) {
   console.log(i);
 }
 
 //Вывести общее кол-во элементов в консоль
-console.log("Count <li>: " + ulElement.length);
+console.log("Count <li>: " + liElement.length);
 
 //Вытянуть текст из тегов li и записать текст содержащийся в каждом элементе в массив — вывести этот массив в консоль
 function getArrayLi() {
   let arrLi = [];
-  for (let i of ulElement) {
+  for (let i of liElement) {
     arrLi.push(i.innerHTML);
   }
   console.log(arrLi);
